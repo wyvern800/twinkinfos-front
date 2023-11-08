92d4f2f7d28d370f81a67b1ff3470bd1f1a0dcda
Matheus Guilherme Ferreira <matheusguiferreira@hotmail.com>
2023-11-08 03:45:17 -0300

    fix: build

--- src/components/BuildsList/index.tsx
+++ src/components/BuildsList/index.tsx
@@ -293,7 +293,7 @@ export const BuildsList = ({
                       </div>
                     );
                   }
-                  return [buildObjectKey, buildObjectValue];
+                  return <></>;
                 },
               )}
             </>
